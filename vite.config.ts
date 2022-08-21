fa0adf3bb21dd82cf4d0d015a79fa30feafce5fa
Raffaele <mori.raffaele@gmail.com>
2022-08-21 15:22:42 +0100

    Added basic info for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/dices-pwa/',
-  plugins: [svelte()]
+  plugins: [svelte(), VitePWA(
+    {
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Dices PWA',
+        short_name: 'dices pwa',
+        description: 'PWA to handle several dices',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }
+  )]
 })
